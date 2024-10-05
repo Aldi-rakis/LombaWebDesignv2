6779fa3f675761055f64f3eaa2e997f6029529f0
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-10-05 19:58:44 +0700

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <h1 className='text-5xl text-red-300'>Hello World</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
